23e988096cfc2308a6d019185ba4d166ea5fe381
bhoomikaasingh999 <bhoomikasingh4926@gmail.com>
2026-07-15 16:29:24 +0530

    Clean up comments and formatting in seed.py
    Removed comments in Hindi and added spacing for clarity.

--- backend/seed.py
+++ backend/seed.py
@@ -7,15 +7,15 @@ database = client.smart_parking
 slot_collection = database.get_collection("slots")
 
 async def seed_slots():
-    # Purane saare record clear karo
+    
     await slot_collection.delete_many({})
     
     dummy_slots = []
-    # 💥 Loop running till 30
+    
     for i in range(1, 31):
         slot_id = f"A{i}"
         
-        # 👇 BACKEND SYNC: Har 5th slot ko database mein bhi EV aur ₹65 ghoshit karo
+        #
         is_ev = (i % 5 == 0)
         price = 65 if is_ev else 50
         
@@ -27,7 +27,7 @@ async def seed_slots():
             "booked_by": None
         }
         
-        # Test ke liye beech-beech mein kuch slots occupied kar dete hain
+
         if slot_id in ["A4", "A12", "A25"]:
             slot_data["is_occupied"] = True
             slot_data["booked_by"] = "parked_user@gmail.com"
